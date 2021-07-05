be8268576a38d1195e6d1744e45a85a797307954
Neea Rusch <neea.rusch@mobilefirst.me>
2021-07-04 23:32:59 -0400

    update check.py

--- check.py
+++ check.py
@@ -7,28 +7,32 @@ README = "README.md"
 URL_PATTERN = r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s)\']{2,}|www\.[a-zA-Z0-9][' \
               r'a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s)\']{2,' \
               r'}|www\.[a-zA-Z0-9]+\.[^\s)\']{2,})'
-acceptable_headers = [200, 403, 406]
 
-# read the readme
+acceptable_headers = [200, 403, 406]
 readme_text = open(README, "r").read()
 urls = list(set(re.findall(URL_PATTERN, readme_text)))
 total = len(urls)
 tenth = total // 10
 failures = []
 
-# check all urls
-print(f'checking {total} urls...')
+def false_alert(status_code, url):
+    return status_code in acceptable_headers or \
+        # sometimes reddit throws 502 for bots -> bypass
+        'reddit' in url and status_code == 502
+
+print(f'Checking {total} urls...')
 for index, url in enumerate(urls):
     # noinspection PyBroadException
     try:
+        # request headers only
         response = requests.head(
             url, headers={'Accept': '*/*'}, timeout=30.0)
-        # sometimes head request fails
-        if response.status_code not in acceptable_headers:
+        if not false_alert(response.status_code, url):
+            # sometimes head request fails - try get
             response = requests.get(
                 url, headers={'Accept': '*/*'}, timeout=30.0)
-            # if get fails also register it as failure
-            if response.status_code not in acceptable_headers:
+            # when everything fails, register as failure
+            if not false_alert(response.status_code, url):
                 failures.append((response.status_code, url))
     except Exception as e:
         failures.append(('error', url))
