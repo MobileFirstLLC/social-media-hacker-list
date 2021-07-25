e3b04e8aa24b8b1e8b8b9e474a2f0324f7306e31
Neea Rusch <neea.rusch@mobilefirst.me>
2021-07-24 23:18:38 -0400

    update check.py

--- check.py
+++ check.py
@@ -1,81 +1,83 @@
 """check all urls in readme for ~OK (acceptable) response"""
+
 import re
 import sys
-import json
+from json import loads
 from requests import request
 from urllib.parse import urlparse
-from dateutil import parser
+from dateutil.parser import parse
 from datetime import datetime
 
-GH_TOKEN = sys.argv[1]
+TOKEN = sys.argv[1] if len(sys.argv) > 1 else None
+LIMIT = int(sys.argv[2]) if len(sys.argv) > 2 else None
 PATTERN = r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-z' \
           r'A-Z0-9]\.[^\s)\']{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-' \
           r'Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+' \
           r'\.[^\s)\']{2,}|www\.[a-zA-Z0-9]+\.[^\s)\']{2,})'
 
 
-def is_gh_repo(url):
-    return 'github.com' in urlparse(url).netloc and len(urlparse(url).path.split('/')) > 2
+def path(url):
+    return urlparse(url).path.split('/')
 
 
-def is_ok(status_code, domain):
-    return status_code in [200, 403, 406] or \
-           ('reddit.com' in urlparse(domain).netloc and status_code == 502)
+def in_host(url, domain):
+    return domain in urlparse(url).netloc
 
 
-def display_progress(n, progress, total):
-    percent = (n * 100) // total
-    if percent % 10 == 0 and percent > progress:
-        print(f'...{percent} %')
-        return percent
-    return progress
+def ok(status, domain):
+    return status in [200, 403, 406] or \
+           (status == 502 and in_host(domain, 'reddit.com'))
 
 
-def try_request(url, method, retry=0):
-    # noinspection PyBroadException
-    try:
-        response = request(method, url, headers={'Accept': '*/*'})
-        code = response.status_code
-        success = is_ok(code, url)
-        return (success, code) if success or retry > 0 \
-            else try_request(url, "GET", 1)
-    except Exception as e:
-        return False, f'error: ' + str(e)
+def active(latest_change):
+    delta = datetime.now() - latest_change.replace(tzinfo=None)
+    return delta.days <= 365
+
+
+def check_url(url, method, retry=0):
+    response = request(method, url, headers={'Accept': '*/*'})
+    success = ok(response.status_code, url)
+    if success or retry > 0:
+        return success, response.status_code
+    return check_url(url, "GET", 1)
 
 
 def active_repo(url):
-    try:
-        [owner, repo] = urlparse(url).path.split('/')[1:3]
-        api_url = f'https://api.github.com/repos/{owner}/{repo}'
-        response = request('GET', api_url, headers={
-            'Authorization': f'token {GH_TOKEN}',
-            'Accept': 'application/vnd.github.v3+json'})
-        if is_ok(response.status_code, url):
-            last_update = parser.parse(json.loads(response.content)['updated_at'])
-            days_since_last_update = (datetime.now() - last_update.replace(tzinfo=None)).days
-            return (True, "OK") if days_since_last_update <= 365 else (False, "INACTIVE")
+    [owner, repo] = path(url)[1:3]
+    api_url = f'https://api.github.com/repos/{owner}/{repo}'
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    if TOKEN:
+        headers['Authorization'] = f'token {TOKEN}'
+    response = request('GET', api_url, headers=headers)
+    if not ok(response.status_code, url):
         return False, response.status_code
-    except Exception as e:
-        return False, f'error: ' + str(e)
-
-
-def print_failures(failures):
-    output = '\n'.join([f'- {msg}: {url}' for (msg, url) in failures])
-    print(f'{len(failures)} failure(s):\n{output}')
+    if not active(parse(loads(response.content)['updated_at'])):
+        return False, "INACTIVE"
+    return True, "OK"
 
 
 def main():
-    urls = list(set(re.findall(PATTERN, open("README.md", "r").read())))
-    total, failures, progress = len(urls), [], -1
+    readme = open("README.md", "r").read()
+    urls = list(set(re.findall(PATTERN, readme)))[0:LIMIT]
+    fails, total, progress = [], len(urls), 0
 
-    print(f'Checking {total} urls')
+    print(f'Checking {total} entries...')
     for index, url in enumerate(urls):
-        success, code = active_repo(url) if is_gh_repo(url) else try_request(url, "HEAD")
-        if not success:
-            failures.append((code, url))
-        progress = display_progress(index, progress, total)
-    if failures:
-        print_failures(failures)
+        is_repo = in_host(url, 'github.com') and len(path(url)) > 2
+        try:
+            success, code = active_repo(url) if is_repo \
+                else check_url(url, "HEAD")
+            if not success:
+                fails.append((code, url))
+        except Exception as e:
+            fails.append((f'error: {e}', url))
+        percent = (index * 100) // total
+        if percent % 10 == 0 and percent > progress:
+            print(f'...{percent} % ({len(fails)})')
+            progress = percent
+    if fails:
+        output = '\n'.join([f'- {m}: {u}' for m, u in fails])
+        print(f'{len(fails)} failure(s):\n{output}')
         sys.exit(1)
     print(f'no issues')
 
