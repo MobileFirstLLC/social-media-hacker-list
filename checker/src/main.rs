74ba5e3fb1024f5ba0c33bf76836cb3b28fc5d77
Neea Rusch <neea.rusch@mobilefirst.me>
2022-08-01 20:32:28 -0400

    update checker

--- checker/src/main.rs
+++ checker/src/main.rs
@@ -1,12 +1,12 @@
-use std::fs::read_to_string;
-use std::env;
-use regex::Regex;
-use std::collections::HashSet;
-use reqwest::{Error, Client, StatusCode};
-use url::{Url, Host};
-use serde::Deserialize;
-use chrono::DateTime;
 use chrono::offset::Utc;
+use chrono::DateTime;
+use regex::Regex;
+use reqwest::{Client, Error, StatusCode};
+use serde::Deserialize;
+use std::collections::HashSet;
+use std::env;
+use std::fs::read_to_string;
+use url::{Host, Url};
 
 enum CheckResult {
     Success(),
@@ -14,11 +14,16 @@ enum CheckResult {
 }
 
 #[derive(Deserialize)]
-struct Repo { updated_at: DateTime<Utc> }
+struct Repo {
+    updated_at: DateTime<Utc>,
+}
 
 fn is_ok(status: StatusCode, url: &str) -> bool {
-    return status == 200 || status == 403 || status == 406 ||
-        (status == 502 && url.contains("reddit.com/"));
+    return status == 200
+        || status == 403
+        || status == 406
+        || (status == 502 && url.contains("reddit.com/"))
+        || (status == 429 && url.contains("apps.apple.com/"));
 }
 
 fn get_url(url: &str) -> Url {
@@ -46,7 +51,8 @@ async fn check_repo(u: &str, token: &str) -> Result<CheckResult, Error> {
         .header("User-Agent", "url-checker")
         .header("Accept", "application/vnd.github.v3+json")
         .header("Authorization", format!("token {}", token))
-        .send().await?;
+        .send()
+        .await?;
     let status = response.status();
     if is_ok(status, u) {
         let repo: Repo = response.json().await?;
@@ -59,17 +65,21 @@ async fn check_repo(u: &str, token: &str) -> Result<CheckResult, Error> {
 }
 
 async fn check_url(u: &str) -> Result<CheckResult, Error> {
-    let head = Client::new().head(u)
+    let head = Client::new()
+        .head(u)
         .header("Accept", "*/*")
         .header("User-Agent", "url-checker")
-        .send().await?;
+        .send()
+        .await?;
     if is_ok(head.status(), u) {
         return Ok(CheckResult::Success());
     }
-    let get = Client::new().get(u)
+    let get = Client::new()
+        .get(u)
         .header("Accept", "*/*")
         .header("User-Agent", "url-checker")
-        .send().await?;
+        .send()
+        .await?;
     if is_ok(get.status(), u) {
         return Ok(CheckResult::Success());
     }
@@ -79,13 +89,25 @@ async fn check_url(u: &str) -> Result<CheckResult, Error> {
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    let readme = if args.len() > 1 { &args[1] } else { "../README.md" };
+    let readme = if args.len() > 1 {
+        &args[1]
+    } else {
+        "../README.md"
+    };
     let token = if args.len() > 2 { &args[2] } else { "" };
     let pattern = r"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.([a-zA-Z0-9()]){1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
     let contents = read_to_string(readme).expect("File read error!");
     let re = Regex::new(pattern).unwrap();
-    let matches: Vec<&str> = re.find_iter(&contents).filter_map(|cap| { Some(cap.as_str()) }).collect();
-    let urls: Vec<_> = matches.iter().cloned().collect::<HashSet<_>>().into_iter().collect();
+    let matches: Vec<&str> = re
+        .find_iter(&contents)
+        .filter_map(|cap| Some(cap.as_str()))
+        .collect();
+    let urls: Vec<_> = matches
+        .iter()
+        .cloned()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect();
     let total = urls.len();
     let mut fails: Vec<String> = Vec::new();
     let mut progress = 0;
@@ -103,8 +125,8 @@ async fn main() {
             Ok(res) => match res {
                 CheckResult::Error(reason) => fails.push(format!("{} - {}", u, reason)),
                 _ => {}
-            }
-            Err(_) => fails.push(format!("{} - error", u))
+            },
+            Err(_) => fails.push(format!("{} - error", u)),
         }
         index = index + 1;
         let percent = (index * 100) / total;
